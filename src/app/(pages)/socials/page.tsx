6681b55e25f51cd5df615bda08b4c0aaed44086a
ScrambledFox <github@jorislodewijks.nl>
2024-11-05 11:15:26 +0100

    fix: social page styling

--- src/app/(pages)/socials/page.tsx
+++ src/app/(pages)/socials/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+// 'use client';
 
 import Header from '@/components/Header';
 import Socials from '@/components/Socials';
@@ -13,25 +13,25 @@ import Subheader from '@/components/Subheader';
 export default function SocialsPage() {
   return (
     <div className='flex flex-col justify-center items-center mb-16'>
-      <div className='container mb-16 px-2'>
+      {/* Headers */}
+      <div className='container mb-8 px-2'>
         <Header text='Stay up-to-date' className='mb-4' />
-        <Header text='follow dinox' className='mb-12' />
-        <Subheader>
-          Stay up to date for freshly produced tracks and events. Make sure you
-          are never missing out on Dinox news and events.
-        </Subheader>
+        <Header text='follow dinox' className='mb-8' />
+        <div className='w-full flex flex-row justify-center'>
+          <Subheader className='p-4 md:w-[60%]'>
+            Stay up to date for freshly produced tracks and events. Make sure
+            you are never missing out on Dinox news and events.
+          </Subheader>
+        </div>
       </div>
 
+      {/* Body */}
       <div className='px-4 w-full md:w-[50%]'>
         <div className='flex flex-row justify-between mb-12'>
           <Socials />
         </div>
 
-        <div className='flex flex-col mx-4'>
-          <div className='relative left-0 w-full h-[152px]'>
-            <SpotifyEmbed />
-          </div>
-        </div>
+        <SpotifyEmbed />
       </div>
     </div>
   );
