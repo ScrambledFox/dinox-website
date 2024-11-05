de2ac588e886577bfa906ba8894356ed46812eb7
ScrambledFox <github@jorislodewijks.nl>
2024-11-05 11:44:50 +0100

    feat: ultrawide support

--- src/components/UnopinionatedAboutBlock.tsx
+++ src/components/UnopinionatedAboutBlock.tsx
@@ -20,7 +20,7 @@ export default function UnopinionatedAboutBlock({
 }: AboutBlockProps) {
   return (
     <div
-      className={`${className} relative flex flex-col border-t-8 border-white`}
+      className={`${className} relative ultrawide:w-svw flex flex-col border-t-8 border-white`}
     >
       {/* Headers */}
       <div className='relative h-[400px] flex flex-row justify-center items-center'>
@@ -40,9 +40,11 @@ export default function UnopinionatedAboutBlock({
       </div>
 
       {/* Body block */}
-      <div className='md:relative text-justify flex flex-col md:flex-row w-full justify-center align-bottom gap-8 md:gap-32 p-8 md:p-[128px] items-center bg-black'>
-        <div className='flex-1'>{children}</div>
-        <div className='md:flex-1 w-full'>{sideElement}</div>
+      <div className='w-full md:relative bg-black flex flex-row justify-center'>
+        <div className='text-justify flex flex-col md:flex-row w-full justify-center align-bottom gap-8 md:gap-32 p-8 md:p-[128px] items-center ultrawide:w-[2000px]'>
+          <div className='flex-1'>{children}</div>
+          <div className='md:flex-1 w-full'>{sideElement}</div>
+        </div>
       </div>
     </div>
   );
